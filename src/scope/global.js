38444236b7cba7b83f7c949d81cdf2ffc2f827bc
Sebastian Corona <sebastian.corona@optimen.com.mx>
2023-12-06 10:26:38 -0600

    Global y function

---
+++ src/scope/global.js
@@ -0,0 +1,24 @@
+// variables
+
+var a; // declaracion
+var b = 'b'; // declaracion y asignacion
+b = 'bb'; // reasignacion
+var a = 'aa'; // redeclaracion
+
+// Global Scope
+
+var fruit = 'apple'; // global
+
+function bestFruit(){
+    console.log(fruit)
+}
+
+bestFruit();
+
+function countries(){
+    country = 'Colombia';
+    console.log(country)
+}
+
+countries();
+console.log(country)
