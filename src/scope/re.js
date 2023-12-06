f474ae253bb2cf355a42a5e2df18d21f3a1f0a6a
Sebastian Corona <sebastian.corona@optimen.com.mx>
2023-12-06 11:39:39 -0600

    reasignacion y redeclaracion

---
+++ src/scope/re.js
@@ -0,0 +1,33 @@
+var firstName; // Se le asigna undefined
+firstName = 'Sebastian';
+console.log(firstName);
+
+var lastName = 'Corona' //declarar / asignar
+lastName = 'Ana' //reasignar
+console.log(lastName)
+
+var secondName = 'Jose' //declarar /asignar
+var secondName = 'Ana' //redeclarar
+console.log(secondName)
+
+// Let
+
+let fruit = 'apple' //declarar y asignar
+fruit = 'kiwi' //reasignar
+//let fruit = 'Banana' //redeclaracion
+// Con let ya no se puede redeclarar
+console.log(fruit)
+
+// Const
+const animal = 'dog' //declaracion y asignacion
+//animal = 'cat' //reasignacion
+// const no deja reasignar
+//const animal = 'cat' //redeclaracion
+// const no deja redeclarar
+console.log(animal)
+
+const vehicles = [];
+vehicles.push('🚗')
+console.log(vehicles)
+vehicles.pop()
+console.log(vehicles)
