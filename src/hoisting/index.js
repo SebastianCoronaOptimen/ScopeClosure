48ef86514c4b541667ab5ae24076bd4eabbe524d
Sebastian Corona <sebastian.corona@optimen.com.mx>
2023-12-07 09:29:58 -0600

    hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,13 @@
+/*
+var nameOfDog; //undefined
+console.log(nameOfDog)
+var nameOfDog = 'Elmo'
+console.log(nameOfDog);*/
+
+
+function nameOfDog(){
+    console.log(`El mejor perrito es ${elmo}`)
+}
+nameOfDog()
+
+var elmo = 'Sebas'
